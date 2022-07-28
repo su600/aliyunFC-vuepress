94a1a2ba34a3c7c5dc8a21f56c120ed7c5683eeb
Su600 <su600@live.com>
2022-07-28 14:49:28 +0800

    update MD files

--- code/docs/.vuepress/config.js
+++ code/docs/.vuepress/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  title: 'VuePress',
+  title: 'Su600',
   description: 'Just playing around',
   themeConfig: {
     nav: [
       { text: '指南', link: '/intro/' },
       { text: '配置', link: '/basic-config/' },
-      { text: 'Github', link: 'https://google.com' },
+      { text: 'Github', link: 'https://github.com/su600' },
     ],
     sidebar: [
       '/',
